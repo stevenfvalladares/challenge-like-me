437f327800d3ea9c714665944aab6881da8b2035
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-04 17:53:51 -0300

    The development of the index.js file is finished

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { getPosts, addPosts } = require("./consultas");
+const { getPosts, addPosts, addLikes, deletePosts } = require("./consultas");
 const express = require("express");
 const app = express();
 
@@ -15,18 +15,54 @@ app.get("/", (req, res) => {
 });
 
 app.get("/posts", async (req, res) => {
-  const posts = await getPosts();
-  const data = posts.map((post) => ({
-    id: post.id,
-    titulo: post.titulo,
-    img: post.img,
-    descripcion: post.descripcion,
-  }));
-  res.json(data);
+  try {
+    const posts = await getPosts();
+    const data = posts.map((post) => ({
+      id: post.id,
+      titulo: post.titulo,
+      img: post.img,
+      descripcion: post.descripcion,
+    }));
+    res.json(data);
+  } catch (error) {
+    res.json({ message: `An error has occurred!` });
+  }
 });
 
 app.post("/posts", async (req, res) => {
-  const { titulo, imageUrl, descripcion, likes } = req.body;
-  await addPosts(titulo, imageUrl, descripcion, likes);
-  res.send("Post added successfully");
+  try {
+    const { titulo, imageUrl, descripcion, likes } = req.body;
+    await addPosts(titulo, imageUrl, descripcion, likes);
+    res.send("Post added successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.put("/posts/likes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await addLikes(id);
+    res.status(200).json({
+      message: "Like added successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await deletePosts(id);
+    res.status(200).json({
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
